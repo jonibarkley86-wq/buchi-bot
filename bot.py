5a93855b1f59a3a5c1779420cecc8036f35181f5
jonibarkley86-wq <jonibarkley86@gmail.com>
2026-05-24 00:47:08 +0300

    Refactor greeting function and update video download

--- bot.py
+++ bot.py
@@ -7,43 +7,47 @@ import os
 bot = telebot.TeleBot("8405392398:AAFcN5SpNwH3rRkHLFUZgPyEQ9LUq94UfzM")
 user_last_greeted = {}
 
+# ሰላምታ ተግባር (እንደነበረ)
 def get_greeting(hour):
     if 5 <= hour < 12:
-        return random.choice(["እንዴት አደርሽ የኔ ልዕልት! 😍", "ደህና አደርሽ የኔ ውድ! ❤️", "ጠዋትሽ እንደ ፀሐይ ይደምቅ የኔ ቆንጆ፣ ምን ልታዘዝልሽ? 💖", "የጠዋት አበባዬ፣ እንዴት አደርሽልኝ? 🌹"])
+        return random.choice(["እንዴት አደርሽ የኔ ልዕልት! 😍", "ደህና አደርሽ የኔ ውድ! ❤️", "ጠዋትሽ እንደ ፀሐይ ይደምቅ የኔ ቆንጆ፣ ምን ልታዘዝልሽ? 💖"])
     elif 12 <= hour < 18:
-        return random.choice(["እንዴት ዋልሽ የኔ ትንሽዬ መልአክ? 😍", "የኔ ቆንጆ እንዴት ዋልሽልኝ? ❤️", "የቀኑን ውሎሽን አሳምርልሽ፣ ምን ላድርግልሽ የኔ ውድ? 💖", "የቀኔ ብርሃን፣ እንዴት ዋልሽ? ☀️"])
+        return random.choice(["እንዴት ዋልሽ የኔ መልአክ? 😍", "የኔ ቆንጆ እንዴት ዋልሽልኝ? ❤️", "ምን ላድርግልሽ የኔ ውድ? 💖"])
     else:
-        return random.choice(["እንዴት አመሸሽ የኔ አበባ! 😍", "የማታው ውበት የኔ ፍቅር! ❤️", "እንዴት አመሸሽ የኔ መልአክ! 💖", "የማታው ኮከቤ፣ እንዴት አመሸሽ? ⭐"])
+        return random.choice(["እንዴት አመሸሽ የኔ አበባ! 😍", "የማታው ውበት የኔ ፍቅር! ❤️", "እንዴት አመሸሽ የኔ መልአክ! 💖"])
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, get_greeting((datetime.datetime.utcnow().hour + 3) % 24))
 
-@bot.message_handler(func=lambda message: message.text.startswith('http'))
+@bot.message_handler(func=lambda message: message.text and message.text.startswith('http'))
 def handle_link(message):
     chat_id = message.chat.id
-    if user_last_greeted.get(chat_id) != datetime.date.today():
-        bot.send_message(chat_id, get_greeting((datetime.datetime.utcnow().hour + 3) % 24))
-        user_last_greeted[chat_id] = datetime.date.today()
+    msg = bot.reply_to(message, "✈️ ቪዲዮሽን በርሬ ሄጄ ላምጣ፣ ጥቂት ሰከንድ ጠብቂኝ! 🕒✨")
     
-    msg = bot.reply_to(message, "✈️ ቪዲዮሽን በፍጥነት እያመጣሁልሽ ነው...")
     try:
-        # በፍጥነት ለማውረድ የሚያስችሉ ኦፕሽኖች
+        # ለተሻለ ጥራት እና ምስል እንዲታይ የተስተካከለ ማውረጃ
         ydl_opts = {
-            'format': 'best', 
-            'outtmpl': 'video.mp4', 
+            'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+            'outtmpl': f'video_{chat_id}.mp4',
             'noplaylist': True,
-            'quiet': True,          # ሎግ እንዳይጽፍ ይረዳል
-            'no_warnings': True     # ማስጠንቀቂያዎችን በመዝለል ፍጥነት ይጨምራል
+            'quiet': True,
+            'no_warnings': True,
+            'merge_output_format': 'mp4' # ምስሉ ጥቁር እንዳይሆን ይረዳል
         }
+        
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([message.text])
         
-        with open('video.mp4', 'rb') as video:
-            bot.send_video(chat_id, video)
+        video_path = f'video_{chat_id}.mp4'
+        with open(video_path, 'rb') as video:
+            # ቪዲዮውን ሲልክ ምስሉ እንዲታይ በደንብ ይልከዋል
+            bot.send_video(chat_id, video, supports_streaming=True)
+        
         bot.send_message(chat_id, "ይሄው የኔ ልዕልት! 😍")
-        os.remove('video.mp4')
-    except Exception as e:
+        os.remove(video_path)
+        
+    except Exception:
         bot.reply_to(message, "አልተሳካም፣ ሊንኩን እንደገና ሞክሪ! 😢")
 
 bot.infinity_polling()
